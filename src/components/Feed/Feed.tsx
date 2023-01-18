36a02c57412919d15e155db95496a96c2013635e
VyacheslavPv <parsh.slav@gmail.com>
2023-01-18 16:07:07 +0200

    Fixed package.json to work with lint-staged and  prettier

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,17 +1,16 @@
-import { useState, useEffect } from "react";
-import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "../SideBar/Sidebar";
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Sidebar from '../SideBar/Sidebar';
 
 const Feed = () => {
   return (
-    <Stack sx={{ flexDirection: { sm: "column", md: "row" } }}>
+    <Stack sx={{ flexDirection: { sm: 'column', md: 'row' } }}>
       <Box
         sx={{
-          height: "92vh",
-          borderRight: "1px solid #3d3d3d",
+          height: '92vh',
+          borderRight: '1px solid #3d3d3d',
           px: { sm: 0, md: 2 },
-        }}
-      >
+        }}>
         <Sidebar />
       </Box>
     </Stack>
--- src/components/SideBar/Sidebar.tsx
+++ src/components/SideBar/Sidebar.tsx
@@ -1,26 +1,24 @@
-import { Stack } from "@mui/material";
+import { Stack } from '@mui/material';
 
-import { categories } from "../../utils/constants";
+import { categories } from '../../utils/constants';
 
 const Sidebar = () => (
   <Stack
     direction="row"
     sx={{
-      overflowY: "auto",
-      height: { sm: "auto", md: "95%" },
-      flexDirection: { md: "column" },
-    }}
-  >
+      overflowY: 'auto',
+      height: { sm: 'auto', md: '95%' },
+      flexDirection: { md: 'column' },
+    }}>
     {categories.map((category) => (
       <button
         className="category-btn"
         onClick={() => {}}
         style={{
-          color: "white",
+          color: 'white',
         }}
-        key={category.name}
-      >
-        <span style={{ marginRight: "15px" }}>{category.icon}</span>
+        key={category.name}>
+        <span style={{ marginRight: '15px' }}>{category.icon}</span>
         <span>{category.name}</span>
       </button>
     ))}
